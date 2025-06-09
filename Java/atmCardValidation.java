1c8a221d1779903722b1d240f98957ac8f61b0a0
John Imeobong <imeobongjohn38@gmail.com>
2025-06-09 12:56:32 +0100

    day2 phase

--- Java/atmCardValidation.java
+++ Java/atmCardValidation.java
@@ -4,7 +4,7 @@ public class atmCardValidation {
 	public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 
-	System.out.println("Enter card number: ");
+	System.out.println("Enter card number to verify: ");
 	int userInput = input.nextInt();
 
 	System.out.println(atmCardValidationFunction.);
--- Python/test_check_out_app.py
+++ Python/test_check_out_app.py
@@ -3,9 +3,22 @@ from check_out_app import CheckOutAppFunctions
 
 class TestCheckOutApp(unittest.TestCase):
 
-	def setUp(self):
+	def test_check_out_app_setUp(self):
 		self.checkout = CheckOutAppFunctions()
 
-	def test_that_check_out_app_exists(self):
-		self.assertIsInstance(self.checkout, CheckOutAppFunctions)
+	def test_that_check_out_app_functions_works(self):
+		self.assertTrue(callable(CheckOutAppFunctions.get_store_details))
+		self.assertTrue(callable(CheckOutAppFunctions.print_customer_invoice))
+		self.assertTrue(callable(CheckOutAppFunctions.print_customer_receipt))
 
+	def test_get_store_details(self):
+		self.assertTrue("\nSEMICOLON STORES \nMAIN BRANCH \nLOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS. \nTEL: 03938373320 \n{datetime.now()}")
+
+	def test_print_customer_invoice(self):
+		checkout = CheckOutAppFunctions("Rice", "2", "1100", "John Floss", "Jose", "0.08")
+		self.assertEqual(checkout.item, "Rice")
+		self.assertEqual(checkout.quantity, 2)
+		self.assertEqual(checkout.price, 1100.0)
+		self.assertEqual(checkout.customer, "John Floss")
+		self.assertEqual(checkout.cashier, "Jose")
+		self.assertEqual(checkout.discount, 0.08)
